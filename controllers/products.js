6135b88f3a8c0a4ec1f9f03026b1a692f9137b07
Mustafaelhdad <Mostafa.eng_2194@eng.kfs.edu.eg>
2023-02-22 23:58:59 +0200

    try sort with the comming data with mongodb but not worked for some resone

--- controllers/products.js
+++ controllers/products.js
@@ -1,24 +1,48 @@
-const Product = require("../models/product")
+const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
+  const search = "";
   const products = await Product.find({
     // featured: true,
+    name: { $regex: search, $options: "i" },
   })
-  res.status(200).json({ products, nbHits: products.length })
-}
+    .sort("-name price")
+    .select("name price");
+  res.status(200).json({ products, nbHits: products.length });
+};
 
 const getAllProducts = async (req, res) => {
   // console.log(req.query)
-  const { featured } = req.query
-  const queryObject = {}
+  const { featured, company, name, sort } = req.query;
+  const queryObject = {};
 
   if (featured) {
-    queryObject.featured = featured === "true" ? true : false
+    queryObject.featured = featured === "true" ? true : false;
   }
 
-  console.log(queryObject)
-  const products = await Product.find(queryObject)
-  res.status(200).json({ products, nbHits: products.length })
-}
+  if (company) {
+    queryObject.company = company;
+  }
 
-module.exports = { getAllProductsStatic, getAllProducts }
+  if (name) {
+    // handling the search with names
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  // console.log(queryObject);
+  let result = Product.find(queryObject);
+
+  if (sort) {
+    // products = products.sort()
+    // console.log(sort);
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+  const products = await result;
+
+  res.status(200).json({ products, nbHits: products.length });
+};
+
+module.exports = { getAllProductsStatic, getAllProducts };
